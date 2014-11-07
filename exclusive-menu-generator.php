47c8dd1d021d669e73f876cb3a82e3ee1fd41c63
webmasterintexas <webmasterintexas@gmail.com>
2014-11-07 09:12:57 -0600

    index page initial

---
+++ exclusive-menu-generator.php
@@ -0,0 +1,11 @@
+/*
+ * Plugin Name: Exclusive Menu Generator
+ * Description: Custom menu generator, creates menus in Worpress based on Bootstrap 3 navwalker class. Based on users input, it will autogenerate the code to autoload different menus based on the page or page parent ID.
+ * Author: Jerry Schrader
+ * Author URI: http://www.jerryschrader.info
+ * Date: 11/07/2014
+ * Version: 1.09
+ * License: Private
+*/
+
+
--- index.php
+++ index.php
@@ -1,4 +1,26 @@
 <?php include('header.php'); ?>
+    
     <h1>Custom Wordpress / Bootstrap Menu Generator</h1>
+
+    <section id="main">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-10 col-sm-offset-1">
+                    <h2>Use this form to add pages to a specific menu in your Wordpress. When it is done, you will have a nice menu generated.</h2>
+                    <form action="" method="POST">
+                        <p>
+                            <label for="menu-name">Menu Name</label>
+                            <input type="text" name="menu-name" class="menu-name" />
+                        </p>
+                        <p>
+                            <label for="menu-name">Parent Page ID</label>
+                            <input type="text" name="parent-page-id" class="parent-page-id" />
+                        </p>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
+
 
 <?php include('footer.php'); ?>
