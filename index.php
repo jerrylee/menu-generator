69762322e90a8a2cdc7f4d6cd48a7d49e8d46ba5
webmasterintexas <webmasterintexas@gmail.com>
2014-11-07 10:43:15 -0600

    minor

--- index.php
+++ index.php
@@ -7,14 +7,17 @@
             <div class="row">
                 <div class="col-sm-10 col-sm-offset-1">
                     <h2>Use this form to add pages to a specific menu in your Wordpress. When it is done, you will have a nice menu generated.</h2>
-                    <form action="" method="POST">
+                    <form action="menu.php" method="POST">
                         <p>
                             <label for="menu-name">Menu Name</label>
-                            <input type="text" name="menu-name" class="menu-name" />
+                            <input type="text" name="menu-name-1" class="menu-name" />
                         </p>
                         <p>
                             <label for="menu-name">Parent Page ID</label>
                             <input type="text" name="parent-page-id" class="parent-page-id" />
+                        </p>
+                        <p>
+                            <input type="submit" value="SUBMIT">
                         </p>
                     </form>
                 </div>
